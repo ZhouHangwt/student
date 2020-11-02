f62bf4f09de97db06af401839310f29c1daa1290
zhouhang <zhouhang@163.com>
2020-11-02 17:30:01 +0800

    在家开发签到100%

--- SignIn.java
+++ SignIn.java
@@ -1,10 +1,3 @@
-/*
-* @Author: Marte
-* @Date:   2020-11-02 17:07:19
-* @Last Modified by:   Marte
-* @Last Modified time: 2020-11-02 17:20:12
-*/
-
 public class java {
     private String name;
     public void sign(){
