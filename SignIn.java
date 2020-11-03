5de55252680960a0b1b18fcfc3137eae0948f026
lisi <lisi@163.com>
2020-11-03 09:28:03 +0800

    第二次修改signout

--- SignIn.java
+++ SignIn.java
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 public class SignIn{
-    private String name;
-=======
 public class java {
     private String name;
     public void sign(){
         System.out.println("sign name -"+name);
     }
->>>>>>> f62bf4f09de97db06af401839310f29c1daa1290
 }
